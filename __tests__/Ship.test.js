3fd39930026b52f688eae0af4d4f9c53bc6736e3
Ben-H123 <ben_h321@protonmail.com>
2023-11-22 20:45:22 +0000

    finished port object

--- __tests__/Ship.test.js
+++ __tests__/Ship.test.js
@@ -8,18 +8,30 @@ describe('Ship', () => {
     })
 
     it('has a starting port', () => {
-        const ship = new Ship('Dover');
+        const port = new Port('Dover');
+        const ship = new Ship(port);
 
-        expect(ship.startingPort).toBe('Dover');
+        expect(ship.currentPort).toBe(port);
     })
 
 
     it('can set sail from a port', () => {
-        const ship = new Ship('Dover');
+        const port = new Port('Dover');
+        const ship = new Ship(port);
 
         ship.setSail();
 
         expect(ship.startingPort).toBeFalsy();
+    })
+
+        it('can dock at a different port', () => {
+        const dover = new Port('Dover');
+        const ship = new Ship(dover);
+
+        const calais = new Port('Calais');
+        ship.dock(calais);
+
+        expect(ship.currentPort).toBe(calais);
     })
 
 });
--- src/Ship.js
+++ src/Ship.js
@@ -1,16 +1,19 @@
 function Ship(port) {
     this.startingPort = port;
-    this.previousPort = null
-
-
-}
+    this.previousPort = null;
+    this.currentPort = port;
+};
 
 Ship.prototype.setSail = function (setSail) {
 
-this.startingPort = this.previousPort
+this.startingPort = this.previousPort;
 
-return this.startingPort
-}
+return this.startingPort;
+};
+
+Ship.prototype.dock = function (port) {
+    this.currentPort = port;
+};
 
 
 
