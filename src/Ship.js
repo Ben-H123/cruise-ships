145b4ba06b4d1d8990d0d2cf4ad38465c94e2f0b
Ben-H123 <ben_h321@protonmail.com>
2023-11-23 11:34:52 +0000

    end of itineraries page

--- src/Ship.js
+++ src/Ship.js
@@ -1,18 +1,26 @@
-function Ship(port) {
-    this.startingPort = port;
+function Ship(itinerary) {
+    this.itinerary = itinerary;
+    this.currentPort = itinerary.ports[0];
     this.previousPort = null;
-    this.currentPort = port;
+    
 };
 
-Ship.prototype.setSail = function (setSail) {
+Ship.prototype.setSail = function() {
+    const itinerary = this.itinerary;
+    const currentPortIndex = itinerary.ports.indexOf(this.currentPort);
 
-this.startingPort = this.previousPort;
-
-return this.startingPort;
+    if (currentPortIndex === (itinerary.ports.length - 1)) {
+        throw new Error('End of itinerary reached');
+    }
+    this.previousPort = this.currentPort;
+    this.currentPort = null;
 };
 
-Ship.prototype.dock = function (port) {
-    this.currentPort = port;
+Ship.prototype.dock = function() {
+    const itinerary = this.itinerary;
+    const previousPortIndex = itinerary.ports.indexOf(this.previousPort);
+
+    this.currentPort = itinerary.ports[previousPortIndex + 1];
 };
 
 
