9d4e1bd9e0268ffc67977069c5e300f77fadd616
Ben-H123 <ben_h321@protonmail.com>
2023-11-24 16:02:31 +0000

    ports have ships done

--- src/Port.js
+++ src/Port.js
@@ -1,9 +1,20 @@
-function Port(name) {
+class Port {
+    constructor(name){
     this.name = name;
+    this.ships = [];
 
 }
 
+addShip(ship){
+    this.ships.push(ship);
 
+}
+
+removeShip(ship){
+    this.ships.pop(ship);
+
+}
+}
 
 
 
--- src/Ship.js
+++ src/Ship.js
@@ -2,6 +2,7 @@ function Ship(itinerary) {
     this.itinerary = itinerary;
     this.currentPort = itinerary.ports[0];
     this.previousPort = null;
+    this.currentPort.addShip(this);
     
 };
 
@@ -9,11 +10,17 @@ Ship.prototype.setSail = function() {
     const itinerary = this.itinerary;
     const currentPortIndex = itinerary.ports.indexOf(this.currentPort);
 
+    this.currentPort.removeShip(this);
+
+
     if (currentPortIndex === (itinerary.ports.length - 1)) {
         throw new Error('End of itinerary reached');
     }
     this.previousPort = this.currentPort;
     this.currentPort = null;
+    
+
+
 };
 
 Ship.prototype.dock = function() {
@@ -21,7 +28,9 @@ Ship.prototype.dock = function() {
     const previousPortIndex = itinerary.ports.indexOf(this.previousPort);
 
     this.currentPort = itinerary.ports[previousPortIndex + 1];
+    this.currentPort.addShip(this);
 };
+
 
 
 
