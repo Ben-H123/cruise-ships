416627ccf60d0c818fc98d3080cfd0a403e8406b
Ben-H123 <ben_h321@protonmail.com>
2023-11-25 14:40:18 +0000

    DRY done

--- __tests__/Port.test.js
+++ __tests__/Port.test.js
@@ -1,34 +1,42 @@
 /* globals describe it expect */
 const Port = require('../src/Port.js');
+const Ship = require('../src/Ship.js');
+const Itinerary = require('../src/Itinerary.js');
 
 describe('Port', () => {
+        let dover;
+        let calais;
+        let titanic;
+        let queenMary;
+        let itinerary;
+
+    beforeEach(() => {
+        dover = new Port('Dover');
+        calais = new Port('Calais');
+        itinerary = new Itinerary([dover, calais]);
+        titanic = new Ship(itinerary);
+        queenMary = new Ship(itinerary);
+    })
+
     it('can be instantiated', () => {
         expect(new Port()).toBeInstanceOf(Object);
     })
 
     it('has a name', () => {
-        const port = new Port('Dover');
-        expect(port.name).toEqual('Dover');
+        expect(dover.name).toEqual('Dover');
     })
 
     it('can add a ship', () => {
-        const port = new Port('Dover');
-        const ship = {};
-
-        port.addShip(ship);
-        expect(port.ships).toContain(ship);
+        dover.addShip(titanic);
+        expect(dover.ships).toContain(titanic);
     })
 
     it('can remove a ship', () => {
-        const port = new Port('Dover');
-        const titanic = {};
-        const queenMary = {};
+        calais.addShip(titanic);
+        calais.addShip(queenMary);
+        calais.removeShip(queenMary);
 
-        port.addShip(titanic);
-        port.addShip(queenMary);
-        port.removeShip(queenMary);
-
-        expect(port.ships).toEqual([titanic]);
+        expect(calais.ships).toEqual([titanic]);
     })
 
-})
+    })
